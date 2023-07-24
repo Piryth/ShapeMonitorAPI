cd2240b7caaaed5964ac8c6ebc1273a2b3710bbd
Piryth <Arnaud.Endignous@etu.univ-grenoble-alpes.fr>
2023-07-24 16:05:55 +0200

    Initial commit.

---
+++ src/main/java/com/piryth/shapemonitorapi/ShapeMonitorApiApplication.java
@@ -0,0 +1,13 @@
+package com.piryth.shapemonitorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeMonitorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShapeMonitorApiApplication.class, args);
+    }
+
+}
